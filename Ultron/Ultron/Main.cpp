ae06662f5744a1e77e501a105f525bdbedb5279b
aasifbkhan <38831685+aasifbkhan@users.noreply.github.com>
2019-08-19 20:59:47 +0530

    haar.xmls added

--- Ultron/Ultron/Main.cpp
+++ Ultron/Ultron/Main.cpp
@@ -1,16 +1,21 @@
 #include <sapi.h>
 #include<iostream>
+#include<string>
 #include<opencv2/opencv.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/objdetect.hpp>
 
 using namespace std;
 using namespace cv;
 
 int main()
 {
-	char ch[1000];
-	int a;
+	string str;
+	string fcp;
 	Mat image;
 	ISpVoice* pVoice = NULL;
+	CascadeClassifier faceCascade;
 
 	if (FAILED(::CoInitialize(NULL)))
 		return FALSE;
@@ -20,20 +25,31 @@ int main()
 	{
 			cout << "\n                                                  Hello Everyone..!" << endl;
 			hr = pVoice->Speak(L"\n Hello Everyone..!", SPF_IS_XML, NULL);
-			cout << "                           I'm Ultron The Image Processing Program Made By Aasif Khan." << endl;
+			cout << "                           I'm Ultron The Image Processing Program." << endl;
 			hr = pVoice->Speak(L"I'm Ultron The Image Processing Program.", SPF_IS_XML, NULL);
 			cout << "\n-->> Enter The Path Of Your Image" << endl;
 			hr = pVoice->Speak(L"Enter The Path Of Your Image", SPF_IS_XML, NULL);
-			cin >> ch;
-			image = imread(ch);
+			getline(cin,str);
+			image = imread(str);
 			if (image.empty()) {
 				cout << "Oops..! Image Is Not Found"<<endl;
 				hr = pVoice->Speak(L"Oops..! Image Is Not Found", SPF_IS_XML, NULL);
+				return -1;
 			}
-			else {
-				cout << " I Found Image..!!!";
-				hr = pVoice->Speak(L"I Found Image..!!!", SPF_IS_XML, NULL);
-			}
+			cout << " I Found Image..!!!";
+			hr = pVoice->Speak(L"I Found Image..!!!", SPF_IS_XML, NULL);
+			fcp = "opencv-master/data/haarcascades/haarcascade_frontalface_alt.xml";
+			faceCascade.load(fcp);
+			vector<Rect> faces;
+			faceCascade.detectMultiScale(image, faces);
+
+
+
+
+
+
+
+
 			namedWindow("image", WINDOW_NORMAL);
 			imshow("image", image);
 			waitKey(0);
